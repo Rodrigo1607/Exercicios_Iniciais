c0092c7a2ba6c239a31cd8a09c1c9029dd9b1402
rodrigo-1607@hotmail.com <rodrigo-1607@hotmail.com>
2023-06-15 16:28:30 -0300

    Adicionar arquivos de projeto.

---
+++ Exercicio01/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+         int NumeroDigitado;
+
+         Console.Write("Insira um Número: ");
+
+         NumeroDigitado = Convert.ToInt32(Console.ReadLine()); // Lê e converte para inteiro
+
+            if (NumeroDigitado % 2 == 0)
+            {
+                Console.WriteLine("É Par");
+            }
+            else
+            { 
+                Console.WriteLine("É Ímpar");
+            }
+
+         Console.WriteLine(NumeroDigitado.ToString());
+            
+         Console.ReadKey();
+                   
+
+
+        }
+    }
+}
